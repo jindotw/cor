3894311b44a3b0658483429b4a3f8f5dc5fab900
Paul Lin <paul@innotech.me>
2020-04-15 21:07:01 +0800

    initialized project skeleton

---
+++ src/main/java/idv/paul/java/cor/CorApplication.java
@@ -0,0 +1,13 @@
+package idv.paul.java.cor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/idv/paul/java/cor/CorApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.paul.java.cor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
